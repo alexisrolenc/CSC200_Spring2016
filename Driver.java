e246ffd0b7697a9b34a3c7835baca86764f87f24
Alexis Rolenc <Alexis Rolenc>
2016-04-10 17:15:54 -0500

    second commit

--- Driver.java
+++ Driver.java
@@ -5,12 +5,65 @@ public class Driver
 	{
 		String s = "hello";
 		String s2 = "elephant";
-		//Driver.displayString(s);
-		//Driver.displayWithSpaces(s);
-		Driver.displayInReverse(s);
-		Driver.displayInReverse(s2);
+		String s3 = "Ha44y B1r7hda9";
+		//System.out.println(s2.indexOf('p'));
+		//System.out.println(s2.indexOf('e'));
+		//System.out.println(s2.indexOf('z'));
+		//System.out.println(Driver.indexOf(s2, 'p'));
+		//System.out.println(Driver.indexOf(s2, 'e'));
+		//System.out.println(Driver.indexOf(s2, 'z'));
+		Driver.removeVowels(s2);
+		Driver.removeConsonants(s);
+		Driver.removeDigits(s3);
 	}
 	
+	//returns a new String with all of those vowels
+	//removed from the parameter String
+	//"hello" -> hll
+	static String removeVowels(String s)
+	{
+		String answer = ""; 
+		answer = answer.replaceAll("AaEeIiOoUu", ""); 
+		return answer; 
+
+	}
+	
+	//returns a new String with all of those consonants
+	//removed from the parameter String
+	//"hello" -> eo
+		static String removeConsonants(String s)
+		{
+			String answer = ""; 
+			answer = answer.replaceAll("BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz", ""); 
+			return answer; 
+
+		}
+	
+	//returns a new String with all of those digits
+	//removed from the parameter String
+	//"he11o" -> heo
+		static void removeDigits(String s)
+		{
+			String answer = ""; 
+			answer = answer.replace("1234567890","");	 
+			System.out.println(answer);
+
+		}
+	
+	//return the first occurrence in s where c is found or -1 if
+	//it was not found- we can't use String's indexOf
+	static int indexOf(String s, char c)
+	{
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(s.charAt(i) == c)
+			{
+				return i;
+			}
+		}
+		//if we are still kicking....
+		return -1;
+	}
 	static void displayString(String s)
 	{
 		//should display the contents of the String one character per line
@@ -32,13 +85,13 @@ public class Driver
 	
 	//this method should display on a single the input string
 	//in reverse: "hello" -> "olleh"
-	static void displayInReverse(String s)
+	static String reverseString(String s)
 	{
 		String answer = "";
 		for(int i = s.length() - 1; i >= 0; i = i - 1)
 		{
-			answer = answer + s.charAt(i) + answer;
+			answer = answer + s.charAt(i);
 		}
-		System.out.println(answer);
+		return answer;
 	}
 }
