808c6e340b0d2159c1f92ffe949e88277bbfe51c
Alexis Rolenc <Alexis Rolenc>
2016-04-29 06:18:33 -0500

    mergeSort

--- Driver.java
+++ Driver.java
@@ -2,15 +2,52 @@ import java.util.*;
 
 public class Driver 
 {
-	public static void main(String[] args)
-	{
-		int[] arrayOfNumbers = new int[10];
-		Driver.fillArrayWithRandomInts(arrayOfNumbers);
-		Driver.printIntArray(arrayOfNumbers);
-		Driver.bubbleSort(arrayOfNumbers);
-		Driver.printIntArray(arrayOfNumbers);
-	}
-	
+	public static void main(String[] args) 
+ 	{ 
+ 		int arrayOfNumbers[] = new int [10]; 
+ 		int arrayOfNumbers2[] = new int [10]; 
+ 		Driver.fillArrayWithRandomInts(arrayOfNumbers); 
+ 		Driver.printIntArray(arrayOfNumbers); 
+ 		Driver.mergeSort(arrayOfNumbers, arrayOfNumbers2, 10); 
+ 		Driver.printIntArray(arrayOfNumbers); 
+ 	} 
+ 	static void mergeSort(int a[], int B[], int n) 
+ 	{ 
+ 	    Driver.mergeHelper(a, 0, n, B); 
+ 	}  
+	 
+ 	static void mergeHelper(int a[], int begin, int end, int b[]) 
+ 	{ 
+ 	    if(end - begin == 1) 
+ 	    { 
+ 	    	return; 
+ 	    } 
+ 	    int middle = (end + begin) / 2; 
+ 	    Driver.mergeHelper(a, begin,  middle, b); 
+ 	    Driver.mergeHelper(a, middle,    end, b); 
+ 	    Driver.display(a, begin, middle, end, b); 
+ 	    Driver.copyArray(b, begin, end, a); 
+ 	} 
+ 	static void display(int a[], int begin, int middle, int end, int b[]) 
+ 	{ 
+ 	    int i = begin; 
+ 	    int j = middle; 
+ 	     
+ 	    for (int k = begin; k < end; k++) { 
+ 	        if (i < middle && (j >= end || a[i] <= a[j])) { 
+ 	            b[k] = a[i]; 
+ 	            i = i + 1; 
+ 	        } else { 
+ 	            b[k] = a[j]; 
+ 	            j = j + 1;     
+ 	        } 
+ 	    }  
+ 	}  
+	 static void copyArray(int b[], int begin, int end, int a[]) 
+	 { 
+ 	    for(int i = begin; i < end; i++) 
+        a[i] = b[i]; 
+	 } 
 	static void bubbleSort(int[] arrayOfNumbers) 
 	{ 
 		int i;
